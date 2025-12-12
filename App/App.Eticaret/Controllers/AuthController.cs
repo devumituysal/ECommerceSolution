b73144eb9d058401cce0f07914b2119c513ab337
devumituysal <dev.umituysal@gmail.com>
2025-12-12 12:59:45 +0300

    Implemented user auth and profile actions.
    Added and updated Register, Login, ForgotPassword, Logout, Details, Edit, MyOrders, and MyProducts action methods.

--- App/App.Eticaret/Controllers/AuthController.cs
+++ App/App.Eticaret/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
 ﻿using App.Data.Contexts;
 using App.Data.Entities;
 using App.Eticaret.Models.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 
@@ -83,5 +88,107 @@ namespace App.Eticaret.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [Route("/forgot-password")]
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [Route("/forgot-password")]
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword([FromForm] ForgotPasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Kullanıcı bulunamadı.");
+                return View(model);
+            }
+
+            
+            await SendResetPasswordEmailAsync(user);
+
+            ViewBag.SuccessMessage = "Şifre sıfırlama maili gönderildi. Lütfen e-posta adresinizi kontrol edin.";
+            ModelState.Clear();
+
+            return View();
+        }
+
+        private async Task SendResetPasswordEmailAsync(UserEntity user)
+        {
+            // Gönderici mail bilgileri güncellenmeli
+            const string host = "smtp.gmail.com";
+            const int port = 587;
+            const string from = "mail";
+            const string password = "şifre";
+
+            var resetPasswordToken = Guid.NewGuid().ToString("n");
+            user.ResetPasswordToken = resetPasswordToken;
+            await _dbContext.SaveChangesAsync();
+
+            using SmtpClient client = new(host, port)
+            {
+                Credentials = new NetworkCredential(from, password)
+            };
+
+            MailMessage mail = new()
+            {
+                From = new MailAddress(from),
+                Subject = "Şifre Sıfırlama",
+                Body = $"Merhaba {user.FirstName}, <br> Şifrenizi sıfırlamak için <a href='https://localhost:5001/renew-password/{user.ResetPasswordToken}'>tıklayınız</a>.",
+                IsBodyHtml = true,
+            };
+
+            mail.To.Add(user.Email);
+
+            await client.SendMailAsync(mail);
+        }
+
+        [Route("/renew-password/{verificationCode}")]
+        [HttpGet]
+        public async Task<IActionResult> RenewPassword([FromRoute] string verificationCode)
+        {
+            if (string.IsNullOrEmpty(verificationCode))
+            {
+                return RedirectToAction(nameof(ForgotPassword));
+            }
+
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.ResetPasswordToken == verificationCode);
+
+            if (user is null)
+            {
+                return RedirectToAction(nameof(ForgotPassword));
+            }
+
+            return View();
+        }
+
+        [Route("/renew-password")]
+        [HttpPost]
+        public async Task<IActionResult> RenewPassword([FromForm] object changePasswordModel)
+        {
+            return View();
+        }
+
+        [Route("/logout")]
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await LogoutUser();
+
+            return RedirectToAction(nameof(Login));
+        }
+
+        private async Task LogoutUser()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
     }
 }
